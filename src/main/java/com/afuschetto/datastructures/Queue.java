59b6bb5651d52dacee69e69ed06651dc3d4c63cd
Antonio Fuschetto <a.fuschetto@icloud.com>
2018-10-18 20:13:21 +0200

    Add Queue implementation

---
+++ src/main/java/com/afuschetto/datastructures/Queue.java
@@ -0,0 +1,43 @@
+package com.afuschetto.datastructures;
+
+public class Queue<T> {
+    private int size;
+    private T dataArray[];
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public Queue(int size) {
+        this.size = size;
+        dataArray = (T[]) new Object[size];
+        front = -1;
+        rear = -1;
+    }
+
+    public void enqueue(T item) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Full queue");
+        }
+        front = (front + 1) % size;
+        dataArray[front] = item;
+        ++nItems;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Empty queue");
+        }
+        rear = (rear + 1) % size;
+        T item = dataArray[rear];
+        --nItems;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return (0 == nItems);
+    }
+
+    public boolean isFull() {
+        return (size == nItems);
+    }
+}
