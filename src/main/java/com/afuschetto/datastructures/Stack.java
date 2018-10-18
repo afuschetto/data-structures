c7a5001331408425bf4c75b78e0c2323ce86eeca
Antonio Fuschetto <a.fuschetto@icloud.com>
2018-10-18 20:12:45 +0200

    Add Stack implementation

---
+++ src/main/java/com/afuschetto/datastructures/Stack.java
@@ -0,0 +1,35 @@
+package com.afuschetto.datastructures;
+
+public class Stack<T> {
+    private int size;
+    private T dataArray[];
+    private int top;
+
+    public Stack(int size) {
+        this.size = size;
+        dataArray = (T[]) new Object[size];
+        top = -1;
+    }
+
+    public void push(T item) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Full stack");
+        }
+        dataArray[++top] = item;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Empty stack");
+        }
+        return dataArray[top--];
+    }
+
+    public boolean isEmpty() {
+        return -1 == top;
+    }
+
+    public boolean isFull() {
+        return (size - 1) == top;
+    }
+}
